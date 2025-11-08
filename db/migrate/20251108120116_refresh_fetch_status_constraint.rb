bfc32940d47baa424bd17e29a964d9f5432c17db
dchuk <darrindemchuk@gmail.com>
2025-11-08 12:01:57 -0800

    db: refresh fetch status constraint

---
+++ db/migrate/20251108120116_refresh_fetch_status_constraint.rb
@@ -0,0 +1,29 @@
+class RefreshFetchStatusConstraint < ActiveRecord::Migration[8.0]
+  ALLOWED_STATUSES = %w[idle queued fetching failed].freeze
+  PREVIOUS_STATUSES = %w[idle queued fetching].freeze
+
+  def up
+    replace_constraint(ALLOWED_STATUSES)
+  end
+
+  def down
+    replace_constraint(PREVIOUS_STATUSES)
+  end
+
+  private
+
+  def replace_constraint(statuses)
+    quoted_values = statuses.map { |status| ActiveRecord::Base.connection.quote(status) }.join(", ")
+
+    execute <<~SQL
+      ALTER TABLE feed_monitor_sources
+      DROP CONSTRAINT IF EXISTS check_fetch_status_values
+    SQL
+
+    execute <<~SQL
+      ALTER TABLE feed_monitor_sources
+      ADD CONSTRAINT check_fetch_status_values
+      CHECK (fetch_status IN (#{quoted_values}))
+    SQL
+  end
+end
